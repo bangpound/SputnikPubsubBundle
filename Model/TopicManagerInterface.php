441325b41036e927fb9e9df61787cdfb73532a96
Dmitri Lakachauskis <lakiboy83@gmail.com>
2013-03-16 20:42:42 +0200

    Clean TopicManagerInterface, extend from latest sputnik/common.

--- Model/TopicManagerInterface.php
+++ Model/TopicManagerInterface.php
@@ -2,8 +2,43 @@
 
 namespace Sputnik\Bundle\PubsubBundle\Model;
 
-use Sputnik\Common\Manager\ManagerInterface;
-
-interface TopicManagerInterface extends ManagerInterface
+interface TopicManagerInterface
 {
+    /**
+     * @return TopicInterface
+     */
+    public function create();
+
+    /**
+     * @param string $id
+     *
+     * @return TopicInterface|null
+     */
+    public function find($id);
+
+    /**
+     * @param array $criteria
+     *
+     * @return TopicInterface|null
+     */
+    public function findOneBy(array $criteria);
+
+    /**
+     * @param TopicInterface $topic
+     *
+     * @return boolean
+     */
+    public function persist($topic);
+
+    /**
+     * @param TopicInterface $topic
+     *
+     * @return boolean
+     */
+    public function remove($topic);
+
+    /**
+     * Flush changes.
+     */
+    public function flush();
 }
